61a84172a2c03a70cf0d8814d9232c8937d8e8e3
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2023-12-21 02:24:55 -0300

    chore(project): add initial deps and settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      connectionName: 'main',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.getOrThrow('DB_URI');
+
+        return {
+          uri,
+        };
+      },
+    }),
+    GatewayModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configServer = app.get<ConfigService>(ConfigService);
+  const serverPort = configServer.getOrThrow('SERVER_PORT');
+
+  const TITLE = 'Project - WhatsApp Clone Backend';
+  const DESCRIPTION = 'The main API of "WhatsApp Clone Backend"';
+  const API_VERSION = '1.0';
+
+  const config = new DocumentBuilder()
+    .setTitle(TITLE)
+    .setDescription(DESCRIPTION)
+    .setVersion(API_VERSION)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  SwaggerModule.setup('api', app, document, { customSiteTitle: TITLE });
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  await app.listen(serverPort);
 }
 bootstrap();
