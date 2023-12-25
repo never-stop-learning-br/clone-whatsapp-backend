8bd86cf058f93e7aad61f62a08afb02262d83e88
Gabriel Corrêa Diniz <53409573+gabrielcdiniz@users.noreply.github.com>
2023-12-25 14:16:34 -0300

    Settings - Initial Settings (#2)
    * chore(husky): add husky initial settings
    
    * chore(husky): add "prepare-commit-msg" hook
    
    * chore(package): add initial necessary deps
    
    * chore(project): add initial deps and settings
    
    * chore(paths): add alias paths settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { GatewayModule } from '@/gateway';
+import { ModulesModule } from '@/modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      connectionName: 'main',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.getOrThrow('DB_URI');
+
+        return {
+          uri,
+        };
+      },
+    }),
+    GatewayModule,
+    ModulesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
