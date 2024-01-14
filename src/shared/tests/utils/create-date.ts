adb8c8dd8585bded265a8d87b898bbb75635aa2a
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-01-14 14:53:08 -0300

    refactor(shared): remove unnecessary code
    - remove some comments
    - remove unnecessary lines

--- src/shared/tests/utils/create-date.ts
+++ src/shared/tests/utils/create-date.ts
@@ -1,22 +1,10 @@
-const cls = console.clear;
-const log = console.log;
-
-cls();
-
-//* ******************************************************* */
-export function createToday(date?: Date | number) {
+function createToday(date?: Date | number) {
   const d = new Date(date || Date.now());
   const day = d.getDate();
   const month = d.getMonth() + 1;
   const year = d.getFullYear();
 
-  log({ day, month, year });
-
   return new Date(`${[year, month, day].join('-')}`);
 }
 
-// const today = createToday();
-// const today = createToday(new Date('2024-1-8'));
-const today = createToday(new Date(2024, 0, 8));
-
-log({ today });
+export { createToday };
